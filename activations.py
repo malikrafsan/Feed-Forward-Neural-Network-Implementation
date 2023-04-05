fix: temp fix for softmax

activations.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def softmax(x):
-    return np.exp(x) / np.sum(np.exp(x), axis=0)    
+    return np.exp(x)  
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))